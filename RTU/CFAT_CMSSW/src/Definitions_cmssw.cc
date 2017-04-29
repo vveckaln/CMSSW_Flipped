36a0f490e34603ad8b4c2f37eac5d86784ac03da
Viesturs Veckalns <viesturs.veckalns@cern.ch>
2017-04-29 16:04:37 +0200

    remove CFAT_CMSSW

--- RTU/CFAT_CMSSW/src/Definitions_cmssw.cc
+++
@@ -1,3 +0,0 @@
-#include "Definitions.hh"
-const unsigned char   Definitions::N_levels_types_                             = 2;                                                                                                                      
-const char          * Definitions::tag_levels_[N_levels_types_]                = {"reco", "gen"};                                                                                                              
